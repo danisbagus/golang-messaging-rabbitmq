9fcfb16a24f34a45deb2ac9075a537f40f7093aa
danisbagus <danisbagussetiawan@gmail.com>
2021-10-04 16:43:09 +0700

    Clean up mail service

--- mail-service/main.go
+++ mail-service/main.go
@@ -13,14 +13,14 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type Request struct {
-	to      []string
-	subject string
-	body    string
-}
-
 type TemplateEmail struct {
 	Name          string
+	TransactionID string
+}
+
+type MailTransactionPaidData struct {
+	Name          string
+	Email         string
 	TransactionID string
 }
 
@@ -31,12 +31,6 @@ func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
-	}
-
-	// set request data
-	reqMail := &Request{
-		to:      []string{"danishter22@gmail.com"},
-		subject: "Transaction Paid",
 	}
 
 	// messaging client driver
@@ -63,61 +57,63 @@ func main() {
 			}
 
 			customerName := fmt.Sprintf("%v", msg["customer_name"])
+			customerEmail := fmt.Sprintf("%v", msg["customer_email"])
 			transactionID := fmt.Sprintf("%v", msg["transaction_id"])
 
-			// set template data
-			templateData := TemplateEmail{
+			mailData := MailTransactionPaidData{
 				Name:          customerName,
+				Email:         customerEmail,
 				TransactionID: transactionID,
 			}
 
-			// parse template
-			errParse := reqMail.ParseTemplate("mail-service/transaction_paid.html", templateData)
-			if errParse != nil {
-				log.Fatal("Error while parse the template", err.Error())
-			}
-
-			// send email
-			errSend := reqMail.SendEmail()
+			errSend := SendMail(mailData)
 			if errSend != nil {
 				log.Fatal("Error while send email", errSend.Error())
 			}
-
-			log.Println("Successfully to send email")
-
 		}
 	}()
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+
 	<-forever
 
 }
 
-func (r *Request) SendEmail() error {
-	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-	from := "From: " + os.Getenv("CONFIG_SENDER_NAME") + "\n"
-	subject := "Subject: " + r.subject + "!\n"
-	msg := []byte(from + subject + mime + "\n" + r.body)
+func SendMail(data MailTransactionPaidData) error {
+	targetEmail := []string{data.Email}
+	subjectEmail := "Transaction Paid"
 
-	smtpAddr := fmt.Sprintf("%s:%s", os.Getenv("CONFIG_SMTP_HOST"), os.Getenv("CONFIG_SMTP_PORT"))
-	auth := smtp.PlainAuth("", os.Getenv("CONFIG_AUTH_EMAIL"), os.Getenv("CONFIG_AUTH_PASSWORD"), os.Getenv("CONFIG_SMTP_HOST"))
-
-	if err := smtp.SendMail(smtpAddr, auth, os.Getenv("CONFIG_AUTH_EMAIL"), r.to, msg); err != nil {
-		return err
+	templateData := TemplateEmail{
+		Name:          data.Name,
+		TransactionID: data.TransactionID,
 	}
-	return nil
-}
 
-func (r *Request) ParseTemplate(templateFileName string, data interface{}) error {
-	t, err := template.ParseFiles(templateFileName)
+	t, err := template.ParseFiles("mail-service/transaction_paid.html")
 	if err != nil {
 		return err
 	}
 	buf := new(bytes.Buffer)
-	if err = t.Execute(buf, data); err != nil {
+	if err = t.Execute(buf, templateData); err != nil {
 		return err
 	}
-	r.body = buf.String()
+
+	emailBody := buf.String()
+
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	from := "From: " + os.Getenv("CONFIG_SENDER_NAME") + "\n"
+	subject := "Subject: " + subjectEmail + "!\n"
+	msg := []byte(from + subject + mime + "\n" + emailBody)
+
+	smtpAddr := fmt.Sprintf("%s:%s", os.Getenv("CONFIG_SMTP_HOST"), os.Getenv("CONFIG_SMTP_PORT"))
+	auth := smtp.PlainAuth("", os.Getenv("CONFIG_AUTH_EMAIL"), os.Getenv("CONFIG_AUTH_PASSWORD"), os.Getenv("CONFIG_SMTP_HOST"))
+
+	if err := smtp.SendMail(smtpAddr, auth, os.Getenv("CONFIG_AUTH_EMAIL"), targetEmail, msg); err != nil {
+		return err
+	}
+
+	log.Println("Successfully to send email")
+
 	return nil
+
 }
 
 func Deserialize(b []byte) (Message, error) {
